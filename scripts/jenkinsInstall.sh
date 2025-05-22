ac17434f0f33f095f59f692fc10d707dcc17321d
Racha <acharoberto6@gmail.com>
2025-05-22 21:17:13 +0530

    Updated user script to add docker installation

--- scripts/jenkinsInstall.sh
+++ scripts/jenkinsInstall.sh
@@ -9,8 +9,8 @@ apt update
 echo "Installing default jdk . . ."
 apt install -y default-jdk
 
-echo "Installing wget and curl"
-apt install -y wget curl
+echo "Installing other required packages"
+apt install -y wget curl ca-certificates
 
 echo "Adding jenkins repo and key . . ."
 wget -O /etc/apt/keyrings/jenkins-keyring.asc \
@@ -26,11 +26,23 @@ apt update
 echo "Installing jenkins . . ."
 apt install -y jenkins
 
-echo "Starting and enabling jenkins service . . ."
+echo "Starting and enabling jenkins . . ."
 systemctl enable --now jenkins
 
-echo "Waiting for jenkins to start . . ."
-sleep 120
+
+echo "Installing docker . . ."
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+echo "Starting and enabling docker . . ."
+systemctl enable --now docker
 
 echo "Initial admin password . . ."
 echo "#############################################"
